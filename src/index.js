c1c400cd1fa922616166b607bfdc7f5e2c316c2a
Dzmitry_A <abdiev.dn@yandex.by>
2020-10-03 03:26:57 +0300

    fix: min max functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 exports.min = function min (arr) {
   let minNumber = arr[0];
-  if (minNumber === undefined) return 0;
+
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[i - 1]) minNumber = arr[i];
+    if (arr[i] < minNumber) minNumber = arr[i];
   }
   return minNumber;
 }
 
 exports.max = function max (arr) {
   let maxNumber = arr[0];
-  if (maxNumber === undefined) return 0;
+
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[i - 1]) maxNumber = arr[i];
+    if (arr[i] > maxNumber) maxNumber = arr[i];
   }
   return maxNumber;
 }
 
 exports.avg = function avg (arr) {
   let sum = 0;
-  let number = arr[0];
-  if (number === undefined) return 0;
+
   for (let i = 0; i < arr.length; i++) {
     sum = sum + arr[i];
   }
