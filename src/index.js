406eac30abdc570d35532676fe254d43189e2bc2
Dzmitry_A <abdiev.dn@yandex.by>
2020-10-03 03:48:59 +0300

    fix: all functions fixed

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 exports.min = function min (arr) {
+  if (arr === undefined || arr.length === 0) return 0;
   let minNumber = arr[0];
-
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] < minNumber) minNumber = arr[i];
   }
   return minNumber;
 }
 
 exports.max = function max (arr) {
+  if (arr === undefined || arr.length === 0) return 0;
   let maxNumber = arr[0];
-
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > maxNumber) maxNumber = arr[i];
   }
   return maxNumber;
 }
 
 exports.avg = function avg (arr) {
+  if (arr === undefined || arr.length === 0) return 0;
   let sum = 0;
-
+  let number = arr[0];
   for (let i = 0; i < arr.length; i++) {
     sum = sum + arr[i];
   }
